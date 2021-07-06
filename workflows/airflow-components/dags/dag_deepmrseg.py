31c261b3449f8daac67a55716d1edb9d4a21ad62
Ashish <ashishsingh@uphs.upenn.edu>
2021-07-06 10:20:57 -0400

    Add modified pipeline

--- workflows/airflow-components/dags/dag_deepmrseg.py
+++ workflows/airflow-components/dags/dag_deepmrseg.py
@@ -43,13 +43,26 @@ dmrs = DeepMRSegOperator(dag=dag, input_operator=convert)
 #    alg_name=alg_name, 
 #    series_description=f'{alg_name}')
                                        
-put_to_minio = LocalMinioOperator(dag=dag, action='put', action_operators=[dmrs], file_white_tuples=('.nrrd'))
+#put_to_minio = LocalMinioOperator(dag=dag, action='put', action_operators=[dmrs], file_white_tuples=('.nrrd'))
 clean = LocalWorkflowCleanerOperator(dag=dag,clean_workflow_dir=False)
 
 # Send DICOM segmentation objects to pacs
 #dcmseg_send = DcmSendOperator(dag=dag, input_operator=nrrd2dcmSeg_invertimage)
 
-get_input >> convert >> dmrs >> put_to_minio >> clean
+#the below pipeline works and sends nrrd to minio
+#get_input >> convert >> dmrs >> put_to_minio >> clean
+
 #get_input >> convert >> invert >> nrrd2dcmSeg_invertimage >> dcmseg_send >> clean
 
+#-----------
+#trying to convert nrrd to dicom seg object and send to pacs
+alg_name = "SkullStripping"
+nrrd2dcmSeg_brain = Itk2DcmSegOperator(dag=dag, 
+    segmentation_operator=dmrs, 
+    single_label_seg_info="Brain",
+    alg_name=alg_name, 
+    series_description=f'{alg_name} - Brain')
 
+dcmseg_send_brain = DcmSendOperator(dag=dag, input_operator=nrrd2dcmSeg_brain)
+
+get_input >> convert >> dmrs >> nrrd2dcmSeg_brain >> dcmseg_send_brain >> clean
