705f1689d15df18c4dcf3e0550b87d7e92621942
Ashish <ashishsingh@uphs.upenn.edu>
2021-07-26 13:26:56 -0400

    updating deepmrseg operator for v0.1.1-july

--- workflows/airflow-components/dags/deepmrseg/DeepMRSegOperator.py
+++ workflows/airflow-components/dags/deepmrseg/DeepMRSegOperator.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 from kaapana.operators.KaapanaBaseOperator import KaapanaBaseOperator, default_registry, default_project
-
+from kaapana.kubetools.resources import Resources as PodResources
 
 class DeepMRSegOperator(KaapanaBaseOperator):
 
@@ -12,10 +12,14 @@ class DeepMRSegOperator(KaapanaBaseOperator):
                  *args, **kwargs
                  ):
 
+        pod_resources = PodResources(request_memory=None, request_cpu=None, limit_memory=None, limit_cpu=None, limit_gpu=None)
+
         super().__init__(
             dag=dag,
             name='deepmrseg',
             image="{}{}/deepmrseg:0.1.0".format(default_registry, default_project),
+            gpu_mem_mb=5500,
+            pod_resources=pod_resources,
             image_pull_secrets=["registry-secret"],
             execution_timeout=execution_timeout,
             *args,
